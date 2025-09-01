77275a346cc45ca62c2d5c24101b991420944855
fw1088 <fw1088@163.com>
2025-09-01 08:24:45 -0700

    chore: bump version to 0.0.5
    - Update version in mvi/build.gradle.kts
    - Update version references in README.md

--- mvi/build.gradle.kts
+++ mvi/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.greathouse"
                 artifactId = "mvi"
-                version = "0.0.4"
+                version = "0.0.5"
                 
                 pom {
                     name.set("MVI Library")
